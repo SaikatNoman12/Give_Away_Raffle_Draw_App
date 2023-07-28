b8e68120c9230f68c022ad2c034b056c8f72eef1
saikatnoman12 <noommaa240@gmail.com>
2023-07-28 18:45:45 +0600

    design complete

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
-const height = document.getElementById('list');
-const listHeight = document.getElementById('listHeight');
-const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function(){
-    if(height.clientHeight > 0){
-        height.style.height = `0px`;
-    }
-    else{
-        height.style.height = `${listHeight.clientHeight + 2}px` ;
-    }
-});
+window.onload = function(){
+    
+    /* --- This is use for  --- */
+    const height = document.getElementById('list');
+    const listHeight = document.getElementById('listHeight');
+    const btn = document.getElementById('btn');
+    
+    btn.addEventListener('click', function(){
+        height.style.height = height.clientHeight > 0 ? `0px` : `${listHeight.clientHeight + 2}px` ;
+    });
+
+
+
+}
